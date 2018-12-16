d50357a72471bf55eecc5ee8c23953dbf008460b
Charles Daab <charlesjdaab@gmail.com>
2018-12-16 10:15:07 -0500

    updates to deploy on heroku

--- server.js
+++ server.js
@@ -2,51 +2,39 @@ const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
+const express = require('express');
+const path = require('path');
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
 
-  return false;
-};
 
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
 
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  debug("Listening on " + bind);
-};
+const app = express();
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
